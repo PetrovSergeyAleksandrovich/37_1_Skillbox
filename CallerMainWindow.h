effe4932857b25f5630019c02392561fef9edcd6
PetrovSergeyAleksandrovich <ps@dsys.ru>
2022-01-10 00:35:27 +0300

    lesson done

---
+++ CallerMainWindow.h
@@ -0,0 +1,37 @@
+//
+// Created by Poizone on 10.01.2022.
+//
+
+#ifndef INC_37_1_SKILLBOX_CALLERMAINWINDOW_H
+#define INC_37_1_SKILLBOX_CALLERMAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtWidgets/QLineEdit>
+
+class CallerMainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+
+    QLineEdit *lineEdit = nullptr;
+
+    CallerMainWindow(QMainWindow *parent = nullptr) : QMainWindow(parent) {}
+
+public slots:
+    void add0() {lineEdit->setText(lineEdit->text() + "0"); };
+    void add1() {lineEdit->setText(lineEdit->text() + "1"); };
+    void add2() {lineEdit->setText(lineEdit->text() + "2"); };
+    void add3() {lineEdit->setText(lineEdit->text() + "3"); };
+    void add4() {lineEdit->setText(lineEdit->text() + "4"); };
+    void add5() {lineEdit->setText(lineEdit->text() + "5"); };
+    void add6() {lineEdit->setText(lineEdit->text() + "6"); };
+    void add7() {lineEdit->setText(lineEdit->text() + "7"); };
+    void add8() {lineEdit->setText(lineEdit->text() + "8"); };
+    void add9() {lineEdit->setText(lineEdit->text() + "9"); };
+    void makeCall() {lineEdit->setText("CALL"); };
+
+
+};
+
+#endif //INC_37_1_SKILLBOX_CALLERMAINWINDOW_H
--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <QApplication>
 #include <QPushButton>
+#include "CallerMainWindow.h"
 #include "./ui_Skillbox_37_1_lessons.h"
 
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    QMainWindow window(nullptr);
-    Ui:Ui::MainWindow caller;
+    CallerMainWindow window(nullptr);
+    Ui::MainWindow caller;
     caller.setupUi(&window);
+    window.lineEdit = caller.lineEdit;
     window.resize(480, 640);
     window.show();
     return QApplication::exec();
