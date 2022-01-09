625f9fd8d4074d366bc3b4c59183cd9be0b68434
PetrovSergeyAleksandrovich <ps@dsys.ru>
2022-01-10 00:04:25 +0300

    ui file added

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <QApplication>
 #include <QPushButton>
+#include "./ui_Skillbox_37_1_lessons.h"
+
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
-    button.show();
+    QMainWindow window(nullptr);
+    Ui:Ui::MainWindow caller;
+    caller.setupUi(&window);
+    window.resize(480, 640);
+    window.show();
     return QApplication::exec();
 }
